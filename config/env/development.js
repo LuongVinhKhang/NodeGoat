82802956f8e9fc46cad43cc82369a1fe61d322c4
KMS\khangluong <khangluong@kms-technology.com>
2018-07-11 18:08:57 +0700

    Config for me

--- config/env/development.js
+++ config/env/development.js
@@ -6,10 +6,11 @@ module.exports = {
    // That will create the local nodegoat data-store, or restore it to a clean state if it already exists.
 
    // db: "mongodb://localhost:27017/nodegoat",
-
+   db: "mongodb://localhost:27017/nodegoat",
    // If you want to use a MongoLab instance, just sign up for it, create a data-store, in this example we call it nodegoat.
    // and again just run the grunt db-reset:development command
    //db: 'mongodb://<dbuser>:<dbpassword>@<databasename>',
+//    db: 'mongodb://khangluong01:khangluong01@ds018268.mlab.com:18268/nodegoat',
 
    // If you want to debug regression tests, you will need the following which is also in the test config:
    zapHostName: "192.168.56.20",
